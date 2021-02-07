3e97f5187f8665e7f4eb8bbb212e31287977822f
Turkana-thetesterone <tmemmedova47@gmail.com>
2021-02-07 03:30:25 -0500

    Project 17

---
+++ Proje17.java
@@ -0,0 +1,358 @@
+package technoStudy.Proje17;
+import Constant.MyConstants;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class Proje17 {
+//    Setup for your test cases: (Use @BeforeClass method for setup)
+//    Open a browser and navigate to https://test.campus.techno.study/
+//    Dismiss the cookie message by clicking on "Got it!" button
+//    Login by the credentials (username = "daulet2030@gmail.com" and password = "TechnoStudy123@")
+
+    WebDriver driver;
+    WebDriverWait wait;
+    JavascriptExecutor js;
+    Robot robot=new Robot();
+
+    public Proje17() throws AWTException {
+    }
+
+    @Test
+    public void login() {
+        System.setProperty("webdriver.chrome.driver", MyConstants.DRIVER_PATH);
+        driver = new ChromeDriver();
+        driver.get("https://test.campus.techno.study/");
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 7);
+
+        driver.findElement(Selector.cookie).click();
+        driver.findElement(Selector.username).sendKeys("daulet2030@gmail.com");
+        driver.findElement(Selector.password).sendKeys("TechnoStudy123@");
+        driver.findElement(Selector.loginbutton).click();
+
+    }
+
+    // Navigate to Student > Students through the left menu
+    @Test(dependsOnMethods = "login")
+    public void navigate() {
+        waitFor(ExpectedConditions.visibilityOfElementLocated(Selector.studentmenu), "Studentmenu not visible");
+        driver.findElement(Selector.studentmenu).click();
+
+        waitFor(ExpectedConditions.visibilityOfElementLocated(Selector.studentsecond), "Student menu not visible");
+        driver.findElement(Selector.studentsecond).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.firstplus));
+        driver.findElement(Selector.firstplus).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.firstname));
+        driver.findElement(Selector.firstname).sendKeys("Turkana");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.lastname));
+        driver.findElement(Selector.lastname).sendKeys("Mammadova");
+
+        driver.findElement(Selector.genderselect).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.gender2));
+        driver.findElement(Selector.gender2).click();
+
+        driver.findElement(Selector.gradeselect).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.grade1));
+        driver.findElement(Selector.grade1).click();
+
+        driver.findElement(Selector.schooldepselect).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.schooldep1));
+        driver.findElement(Selector.schooldep1).click();
+
+        driver.findElement(Selector.sectionselect).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.section1));
+        driver.findElement(Selector.section1).click();
+
+        driver.findElement(Selector.citizenshipselect).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.citizen1));
+        driver.findElement(Selector.citizen1).click();
+        js = (JavascriptExecutor) driver;
+        WebElement form = driver.findElement(By.cssSelector("[formgroupname=\"documentInfo\"]>mat-form-field:nth-child(1)"));
+        js.executeScript("arguments[0].scrollIntoView();", form);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.doctype));
+        driver.findElement(Selector.doctype).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.docelement1));
+        driver.findElement(Selector.docelement1).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.docnumber));
+        driver.findElement(Selector.docnumber).sendKeys("9009");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.repmainmenu));
+        driver.findElement(Selector.repmainmenu).click();
+       }
+
+
+/*
+   Click on + icon for adding a student
+   Select "Representatives" from the bottom tabs
+   Click on + icon to add a new representative
+
+   Verify that you can add a new representative of Father, Mother, Guardian and Self
+   Write only one test case for verification of creation of representatives
+   Use a single Data Provider in order to use the same test case for creation of each type of representative.
+ */
+
+    @DataProvider(name = "DataProvider")
+    public Object[][] dataProvider1() {
+    Object[][] testData = {
+            {"Tarana","Mammadova","mat-option:nth-child(1)","6145689531"},
+            {"Somename","Somesurname","mat-option:nth-child(2)","8945786543"},
+            {"name","surname","mat-option:nth-child(3)","8945786543"},
+            {"Turkana","Mammadova","mat-option:nth-child(4)","8945786543"},
+    };
+    return testData;
+}
+
+
+    @Test(dependsOnMethods = "navigate",dataProvider ="DataProvider" )
+    public void test1_creating(String Repname, String Repsurname,String Representative, String Repmobil) throws InterruptedException {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.firstplus));
+        driver.findElement(Selector.firstplus).click();
+
+        waitFor(ExpectedConditions.visibilityOfElementLocated(Selector.representativeselect),"Representative is not visible");
+        driver.findElement(Selector.reprfirstname).clear();
+        driver.findElement(Selector.reprfirstname).sendKeys(Repname);
+        driver.findElement(Selector.reprlastname).clear();
+        driver.findElement(Selector.reprlastname).sendKeys(Repsurname);
+
+        driver.findElement(Selector.representativeselect).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.mother));
+        driver.findElement(By.cssSelector(Representative)).click();
+
+
+        driver.findElement(Selector.repmobile).sendKeys(Repmobil);
+
+        waitFor(ExpectedConditions.visibilityOfElementLocated(Selector.repcountryselect),"Countury is not visible");
+        driver.findElement(Selector.repcountryselect).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(Selector.repcountry1));
+        driver.findElement(Selector.repcountry1).click();
+        wait.until(ExpectedConditions.elementToBeClickable(Selector.repadd));
+        driver.findElement(Selector.repadd).click();
+
+        js = (JavascriptExecutor) driver;
+        WebElement form2 = driver.findElement(By.cssSelector(".mat-paginator-container mat-form-field"));
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS) ;
+        js.executeScript("arguments[0].scrollIntoView();", form2);
+
+
+        List<WebElement> rows = driver.findElements(Selector.repcontain);
+        for (WebElement row : rows) {
+            if (!(row.getText().contains(Repname) &&row.getText().contains(Repsurname))){
+                continue;
+            }
+            Assert.assertTrue(row.getText().contains(Repname));
+        }
+
+    }
+
+/*
+  Verify you can delete an existing representative.
+  (Optional: Use only one test case to delete the four representative you have created about.
+    You may use data provider or run your test many times.)
+  (For automated testing/verification, deleting one representative will be enough but
+   please do not forget to delete other representative you have created above, at least manually)
+*/
+    @Test(dependsOnMethods = "test1_creating",dataProvider ="DataProvider")
+    public void test2_deleting(String Repname, String Repsurname,String Representative, String Repmobil)  {
+
+    wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.repcontain));
+    List<WebElement> rows = driver.findElements(Selector.repcontain);
+    boolean res = false;
+    for (WebElement row : rows) {
+        if (row.getText().contains(Repname) && row.getText().contains(Repsurname)) {
+            System.out.println(row.getText());
+            res = true;
+            driver.findElement(Selector.reptrash).click();
+            wait.until(ExpectedConditions.elementToBeClickable(Selector.confirmYes));
+            driver.findElement(Selector.confirmYes).click();
+        }
+        Assert.assertTrue(res);
+
+    }
+
+}
+/*
+   Verify that a representative cannot be created (or add button will not be active)
+   if one of the following data is missing: Representative, First Name, Last Name, Mobile Phone or Country
+   Write a single test case and use a single data provider to test each case with a missing information each time
+*/
+
+    @DataProvider(name = "MissingDataProvider")
+    public Object[][] dataProvider2() {
+        Object[][] testData = {
+                {"","Mammadova","mat-option:nth-child(1)","6145689531","mat-option:nth-child(2)"},
+                {"Somename","","mat-option:nth-child(2)","8945786543","mat-option:nth-child(24)"},
+                {"name","surname","","8945786543","mat-option:nth-child(18)"},
+                {"Turkana","Mammadova","mat-option:nth-child(4)","8945786543",""},
+        };
+        return testData;}
+
+    @Test(dependsOnMethods = "test2_deleting",dataProvider ="MissingDataProvider" )
+    public void test3_missingdata(String Repname, String Repsurname,String Representative, String Repmobil,String Repcountry)  {
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.firstplus));
+        driver.findElement(Selector.firstplus).click();
+        waitFor(ExpectedConditions.visibilityOfElementLocated(Selector.representativeselect),"Representative is not visible");
+        driver.findElement(Selector.reprfirstname).sendKeys(Repname);
+        driver.findElement(Selector.reprlastname).sendKeys(Repsurname);
+
+        driver.findElement(Selector.representativeselect).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.mother));
+        try {
+            driver.findElement(By.cssSelector(Representative)).click();
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+            robot.keyPress(KeyEvent.VK_TAB);
+        }
+
+        driver.findElement(Selector.repcountryselect).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(Selector.repcountry1));
+        try {
+            driver.findElement(By.cssSelector(Repcountry)).click();
+
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+            robot.keyPress(KeyEvent.VK_TAB);
+        }
+
+        driver.findElement(Selector.repmobile).sendKeys(Repmobil);
+        waitFor(ExpectedConditions.elementToBeClickable(Selector.repadd),"Representative can't be add without any required parameter");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.repclose));
+        driver.findElement(Selector.repclose).click();
+
+        List<WebElement> rows = driver.findElements(Selector.repcontain);
+        if(rows.size()>0){
+            Assert.fail();
+        }else {
+            Assert.assertEquals(0,rows.size());
+        }
+
+    }
+
+/*
+   (The following tests must fail. Because you are not supposed to create a representative as stated but in fact you can!)
+   Verify that you cannot create a representative (add button should not be active) with the following data:
+      with a First Name like "*#@%/"
+      with a Last Name like "125436"
+      with a Mobile Phone like "abcdef"
+   Write a single test case for this test and pass parameters from TestNG.xml file to test the three cases.
+*/
+
+    @Parameters({"Repname","Repsurname","Repmobil"})
+    @Test(dependsOnMethods = "test3_missingdata")
+    public void test4_creatingWithRandomChar(String Repname, String Repsurname, String Repmobil) throws InterruptedException {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.firstplus));
+        driver.findElement(Selector.firstplus).click();
+
+        waitFor(ExpectedConditions.visibilityOfElementLocated(Selector.representativeselect),"Representative is not visible");
+        driver.findElement(Selector.reprfirstname).clear();
+        driver.findElement(Selector.reprfirstname).sendKeys(Repname);
+        driver.findElement(Selector.reprlastname).clear();
+        driver.findElement(Selector.reprlastname).sendKeys(Repsurname);
+
+        driver.findElement(Selector.representativeselect).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Selector.mother));
+        driver.findElement(Selector.father).click();
+
+        driver.findElement(Selector.repmobile).sendKeys(Repmobil);
+        waitFor(ExpectedConditions.visibilityOfElementLocated(Selector.repcountryselect),"Countury is not visible");
+        driver.findElement(Selector.repcountryselect).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(Selector.repcountry1));
+        driver.findElement(Selector.repcountry1).click();
+        wait.until(ExpectedConditions.elementToBeClickable(Selector.repadd));
+        driver.findElement(Selector.repadd).click();
+
+        Thread.sleep(1000);
+        js = (JavascriptExecutor) driver;
+        WebElement form2 = driver.findElement(By.cssSelector(".mat-paginator-container mat-form-field"));
+        js.executeScript("arguments[0].scrollIntoView();", form2);
+
+        List<WebElement> rows = driver.findElements(Selector.repcontain);
+        for (WebElement row : rows) {
+            if (!(row.getText().contains(Repname) &&row.getText().contains(Repsurname))){
+                continue;
+            }
+            Assert.assertFalse(row.getText().contains(Repname));
+        }
+
+    }
+
+/*
+    Create a folder called "screenshot" (suggestively in the project package, not somewhere else in your computer.)
+    Create an @AfterMethod to take a screenshot when the test fails.
+    Save the screenshots in the folder called "screenshots"
+    Create @AfterClass to close the browser
+*/
+@AfterMethod
+public void takeScreenshotWhenTastFails(ITestResult result) {
+    if (result.getStatus() == ITestResult.FAILURE) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss-nn");
+        String time = LocalDateTime.now().format(formatter);
+
+        File sourceFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String folderPath = "C:\\Users\\ememm\\Intell\\Testng_B3\\test-output\\Screenshouts";
+        String fileName = result.getMethod().getMethodName() + "_" + time + ".png";
+
+        File destinationFile = new File(folderPath + fileName);
+
+        try {
+            FileUtils.copyFile(sourceFile, destinationFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+        @AfterClass
+        public void closewindows(){
+         driver.quit();
+}
+
+
+
+/*
+    Add a listener to your xml file for getting well-prepared, good-looking and e-mailable test reports (like html reports)
+
+    OPTIONAL: Run your project in parallel (say, running three tests at a time)
+      It is strongly suggested that before trying parallel execution, make sure they run perfectly one after other.
+    CHALLENGE: Without writing any more test cases (you may have to change your code a little bit),
+     run your test in two other browsers (say, Firefox and Edge, or else)
+     Before trying challenge part, make sure your test cases work perfectly with one browser.
+*/
+
+
+    private <T> void waitFor(ExpectedCondition<T> condition) {
+        waitFor(condition, condition.toString());
+    }
+
+    private <T> void waitFor(ExpectedCondition<T> condition, String errorMessage) {
+        try {
+            wait.until(condition);
+        } catch (TimeoutException e) {
+            Assert.fail(errorMessage);
+        }
+    }
+
+}
+
---
+++ Selector.java
@@ -0,0 +1,58 @@
+package technoStudy.Proje17;
+import org.openqa.selenium.By;
+
+public class Selector {
+    public static By cookie = By.cssSelector("a[aria-label=\"dismiss cookie message\"]");
+    public static By username = By.cssSelector("input[data-placeholder=\"Username\"]");
+    public static By password = By.cssSelector("input[data-placeholder=\"Password\"]");
+    public static By loginbutton = By.cssSelector("button[aria-label=\"LOGIN\"]");
+    public static By studentmenu =By.cssSelector(".group-items > :nth-child(4)");
+    public static By studentsecond =By.cssSelector(".group-items > :nth-child(4)>div>:nth-child(1) >a>span");
+    public static By firstplus =By.cssSelector("ms-table-toolbar ms-add-button");
+
+    public static By firstname = By.cssSelector("ms-text-field[formcontrolname='firstName']>input");
+    public static By lastname = By.cssSelector("ms-text-field[formcontrolname=\"lastName\"]>input");
+    public static By genderselect =By.cssSelector("[formcontrolname=\"gender\"]");
+    public static By gender1 =By.cssSelector("[role=\"option\"]:nth-child(1)");
+    public static By gender2 =By.cssSelector("[role=\"option\"]:nth-child(2)");
+    public static By gradeselect =By.xpath("//mat-select//span[text()='Grade Level']");
+    public static By grade1 =By.cssSelector("mat-option:nth-child(2)");
+    public static By grade2 =By.cssSelector("mat-option:nth-child(10)");
+    public static By schooldepselect =By.cssSelector("[formgroupname=\"department\"]");
+    public static By schooldep1 =By.cssSelector("mat-option:nth-child(1)");
+    public static By sectionselect =By.cssSelector("[formcontrolname=\"section\"] span");
+    public static By section1 =By.cssSelector("mat-option:nth-child(1)");
+    public static By citizenshipselect =By.cssSelector("[formgroupname=\"citizenship\"] mat-select span");
+    public static By citizen1 =By.cssSelector("mat-option:nth-child(100)");
+    public static By doctype =By.cssSelector("[formgroupname=\"documentInfo\"]>mat-form-field:nth-child(1)");
+    public static By docelement1 =By.cssSelector("mat-option:nth-child(1)");
+    public static By docnumber =By.cssSelector("input[formcontrolname=\"documentNumber\"]");
+    public static By save =By.cssSelector("ms-save-button > button");
+    public static By backarrow =By.cssSelector("toolbar [data-icon=\"arrow-left\"]");
+
+    public static By repmainmenu =By.xpath("//*[text()='Representatives']");
+    public static By representativeselect =By.xpath("//mat-select//span[text()='Representative']");
+    public static By mother =By.cssSelector("mat-option:nth-child(1)");
+    public static By father =By.cssSelector("mat-option:nth-child(2)");
+    public static By guardian =By.cssSelector("mat-option:nth-child(3)");
+    public static By self =By.cssSelector("mat-option:nth-child(4)");
+
+    public static By reprfirstname =By.cssSelector("[data-placeholder=\"First Name\"]");
+    public static By reprlastname =By.cssSelector("[data-placeholder=\"Last Name\"]");
+    public static By repmobile =By.cssSelector("mat-dialog-content [data-placeholder=\"Mobile Phone\"] ");
+    public static By repcountryselect =By.cssSelector("[formgroupname=\"country\"]  .mat-select-value>span");
+    public static By repcountry1 =By.cssSelector("mat-option:nth-child(2)");
+    public static By repadd =By.xpath("//span[text()=' Add ']");
+    public static By confirmYes = By.cssSelector("button[type='submit']");
+    public static By repcontain =By.cssSelector("[role=\"rowgroup\"] td.mat-column-fullName");
+    public static By repclose =By.xpath("//*[text()=' Close ']");
+    public static By reptrash=By.cssSelector("tbody ms-delete-button>button");
+
+    public static By newaccouninanouther =By.xpath("//span[text()='New']");
+    public static By newrep =By.cssSelector("[tooltip=\"GENERAL.BUTTON.ADD_REPRESENTATIVE\"]");
+    public static By scrollby =By.cssSelector(".mat-paginator-container mat-form-field");
+    public static By popup =By.id("toast-container");
+    public static By editbutton = By.cssSelector("[data-icon=\"edit\"]");
+
+}
+
